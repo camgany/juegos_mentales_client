151a377360bc258fba41440d4631c7c99720e229
camgany <you@example.com>
2022-09-16 23:46:07 -0400

    app created and images added

---
+++ src/pages/Schools.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import buttonImage from "../components/schools/button"
+export const Schools = () => {
+    
+    return (
+        <div>
+        <buttonImage />
+        </div>
+    )
+    }
+    
+
